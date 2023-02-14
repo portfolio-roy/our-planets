0c691b64cba3cd4913d45f5c9a9b03e4ba8595b6
Utsargo Roy <royutsargo@gmail.com>
2023-02-14 09:45:02 +0600

    Initial setup

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero = () => (
+  <div className="container">
+    <div className="jumbotron">
+      <h1 className="display-4">Hello, world!</h1>
+      <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr className="my-4" />
+      <p>Duis ea qui minim deserunt ullamco sint enim aute commodo ex.</p>
+      <p className="lead">
+        <button className="btn btn-primary btn-lg" type="button">Learn more</button>
+      </p>
+    </div>
+  </div>
+);
+
+export default Hero;
---
+++ src/components/Navigation.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Navigation = () => <h2>Here goes the Navigation</h2>;
+
+export default Navigation;
