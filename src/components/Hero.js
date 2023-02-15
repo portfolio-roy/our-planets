5afbbf67ff357ae0e0338727314831012ccd38d3
Utsargo Roy <royutsargo@gmail.com>
2023-02-15 08:53:36 +0600

    Change dwarf planet number in the banner

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => (
           <div className="col-md-4 col-sm-12 banner-text right">
             <h3>Our Solar System</h3>
             <h5>8 planets</h5>
-            <h5>5 dwarf planets</h5>
+            <h5>4 dwarf planets</h5>
             <h5>200+ Moons</h5>
             <h5>1,266,151 Asteroids</h5>
             <h5>3,855 Comets</h5>
